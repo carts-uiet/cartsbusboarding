32663626dd18e0311ec64bbec13814236c3bc239
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-10-23 09:25:57 +0530

    Use GsmListener instead of LocationListner, Cleanup dead code

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/gsm/GsmService.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/gsm/GsmService.java
@@ -3,26 +3,23 @@ package in.ac.iitb.cse.cartsbusboarding.gsm;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.IBinder;
 
 public class GsmService extends Service {
-    LocationManager gpsMgr, gsmMgr;
-    double gsmLat, gsmLong;
-    float gsmAccuracy;
-    Context mContext;
+    LocationManager gsmMgr;
+    GsmData dataRead;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        //GPSmgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         gsmMgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         GsmListner gsmListener = new GsmListner();
 
         gsmMgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, gsmListener);
-        GsmData dataRead = gsmListener.getData();
+        dataRead = gsmListener.getData();
     }
+
 
     @Override
     public IBinder onBind(Intent intent) {

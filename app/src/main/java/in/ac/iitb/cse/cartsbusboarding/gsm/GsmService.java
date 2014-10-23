94bb6795fc0f695ebaafc556f986040ead3d078c
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-10-23 10:47:29 +0530

    Created a communication channel for service

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/gsm/GsmService.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/gsm/GsmService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class GsmService extends Service {
@@ -29,7 +30,23 @@ public class GsmService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        // Return the communication channel to the service.
+        return mBinder;
     }
+
+    /**
+     * Class for clients to access. Because we know this service always runs in
+     * the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+
+        public GsmService getService() {
+            return GsmService.this;
+        }
+    }// LocalBinder
+
+    // This is the object that receives interactions from clients. See
+    // RemoteService for a more complete example.
+    private final IBinder mBinder = new LocalBinder();
+
 }

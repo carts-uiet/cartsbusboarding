ede46455dbe10020425d8e77151d97e621d892a2
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-10-23 10:48:09 +0530

    Connect to service and send data

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/gsm/GsmEngine.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/gsm/GsmEngine.java
@@ -1,16 +1,50 @@
 package in.ac.iitb.cse.cartsbusboarding.gsm;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
 
 /**
  * Created by chaudhary on 10/23/14.
  */
 public class GsmEngine {
+    private ServiceConnection mServiceConnection;
+    GsmService mGsmService;
     Context mContext;
+    GsmData data;
 
     public GsmEngine(Context context) {
         mContext = context;
         mContext.startService(new Intent(mContext, GsmService.class));
+
+        initServiceConnection();
     }
+
+    private void initServiceConnection() {
+        mServiceConnection = new ServiceConnection() {
+
+            public void onServiceConnected(ComponentName className, IBinder service) {
+                try {
+                    mGsmService = ((GsmService.LocalBinder) service).getService();
+                } catch (Throwable t) {
+                    Log.e("GsmEngine", "mServiceConnection.onServiceConnected() -> " + t);
+                }
+            }// onServiceConnected()
+
+            public void onServiceDisconnected(ComponentName className) {
+                mGsmService = null;
+            }// onServiceDisconnected()
+        };
+
+        mContext.bindService(new Intent(mContext, GsmService.class), mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    public GsmData getData() {
+        data = mGsmService.getData();
+        return data;
+    }
+
 }

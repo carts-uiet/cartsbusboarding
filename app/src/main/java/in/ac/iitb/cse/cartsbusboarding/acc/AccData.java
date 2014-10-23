1932eb316dcdcebb59b494553638eb6d8362d8f7
Tanjot Kaur <tanjot28@gmail.com>
2014-10-24 00:40:23 +0530

    Added functions to AccData structure

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/acc/AccData.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/acc/AccData.java
@@ -8,4 +8,24 @@ public class AccData {
      * Data Encapsulation
      */
     float x,y,z;
+    @Override
+    public String toString() {
+        String base = "ACC Data: ";
+        base += getX()+",";
+        base += getY()+",";
+        base += getZ();
+        return base;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getZ() {
+        return z;
+    }
 }

607860a5ed75407b656f0fd47aef938cb1406ee7
Tanjot Kaur <tanjot28@gmail.com>
2014-10-24 11:43:38 +0530

    Calculating mean of acceleration
    	* acc in x, y, z direction
    	* BUG: all values are same

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/acc/AccEngine.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/acc/AccEngine.java
@@ -7,6 +7,11 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.util.Log;
 
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by chaudhary on 10/23/14.
  */
@@ -15,6 +20,7 @@ public class AccEngine {
     AccService mAccService;
     Context mContext;
     AccData data;
+    int queueSize;
 
     public AccEngine(Context context) {
         mContext = context;
@@ -44,7 +50,69 @@ public class AccEngine {
 
     public AccData getData() {
         data = mAccService.getData();
+        Log.e("data","x"+data.getX()+",y"+data.getY()+",z"+data.getZ());
         return data;
     }
+    public Queue[] getData(int noOfReadings){
+        Queue queueX = null;
+        Queue queueY = null;
+        Queue queueZ = null;
 
+        queueSize = 0;
+        queueX = new LinkedList();
+        queueY = new LinkedList();
+        queueZ = new LinkedList();
+
+//        int outOfLoop = 100 + noOfReadings;
+
+        for(int i=0; i<noOfReadings; i++){
+            if(getData() != null){
+                queueX.add(data.getX());
+                queueY.add(data.getY());
+                queueZ.add(data.getZ());
+                queueSize++;
+            }else{
+                i--;
+            }
+
+            int sleepTime = 10;//unit is milliseconds
+            try{
+                Thread.sleep(sleepTime);
+            }catch (Exception e){
+                Log.e("Interrupted error: ",""+e);
+            }
+
+//            TODO:check for infinte loop i.e. if all values obtained are null
+        }
+        Queue queue[] = null;
+
+        if(queueSize > 0){
+            queue = new Queue[3];
+            queue[0] = queueX;
+            queue[1] = queueY;
+            queue[2] = queueZ;
+        }
+        return  queue;
+    }
+
+    public float[] mean(int noOfReadings){
+        Queue queue[] = getData(noOfReadings);
+
+        if(queue == null) return(null);
+
+        float sum[] = new float[3];
+
+        while(!queue[0].isEmpty()){ //assumed that size of x, y, z are same
+            sum[0] += Float.parseFloat((queue[0].remove()).toString());
+            sum[1] += Float.parseFloat((queue[1].remove()).toString());
+            sum[2] += Float.parseFloat((queue[2].remove()).toString());
+        }
+
+        float mean[] = new float[3];
+        mean[0] = sum[0]/queueSize;
+        mean[1] = sum[1]/queueSize;
+        mean[2] = sum[2]/queueSize;
+        Log.e("mean","x:"+mean[0]+",y:"+mean[1]+",z:"+mean[2]);
+        return  mean;
+    }
 }

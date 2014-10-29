9f88dd1ca0ccec212d1b30962e2a3bd4d86be4e4
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-10-30 03:09:01 +0530

    Pattern Recognition class

---
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/acc/PatternRecognition.java
@@ -0,0 +1,16 @@
+package in.ac.iitb.cse.cartsbusboarding.acc;
+
+/**
+ * Created by chaudhary on 10/30/14.
+ */
+public class PatternRecognition {
+    private AccEngine accEngine;
+
+    public PatternRecognition(AccEngine accEngine) {
+        this.accEngine = accEngine;
+    }
+
+    public boolean checkPattern() {
+        return true;
+    }
+}

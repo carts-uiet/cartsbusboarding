c849a8069b424127caca8893633f077e5816c2bf
Tanjot Kaur <tanjot28@gmail.com>
2014-10-30 02:32:15 +0530

    Synchronized bufferToArray instead of calculateMean

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/acc/AccEngine.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/acc/AccEngine.java
@@ -75,9 +75,10 @@ public class AccEngine {
 
     /**
      * Stores the absolute(x,y,z) of current buffer values in a double array
+     * synchronized to sync threads(both access mainBuffer)
      * @return double array corresponding to data in buffer
      */
-    public double[] bufferToArray(){
+    public synchronized double[] bufferToArray(){
         double bufferArray[] = new double[mainBuffer.size()];
 
         int index = 0;
@@ -102,10 +103,9 @@ public class AccEngine {
 
     /**
      * Calculates mean of data in mainBuffer
-     * synchronized to sync threads(both access mainBuffer)
      * @return
      */
-    private synchronized double calculateMean() {
+    private double calculateMean() {
         return (new Mean()).evaluate(bufferToArray());
     }
 
@@ -146,7 +146,7 @@ public class AccEngine {
                     }
                 }//end while
                 Log.d(_ClassName, "Thread Data: " + mainBuffer.size());
-                Log.d(_ClassName, "Thread Mean: " + calculateMean());
+                Log.d(_ClassName, "Thread Mean: " + getMean());
                 try {
                     Thread.sleep(listenerPollingTime);
                 } catch (InterruptedException e) {

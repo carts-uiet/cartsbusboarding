0cf4d7799681d3a8d3390242c13f7df25773561a
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-10-30 16:13:28 +0530

    getDistance function

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/gsm/GsmEngine.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/gsm/GsmEngine.java
@@ -4,6 +4,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.location.Location;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -45,6 +46,27 @@ public class GsmEngine {
     public GsmData getData() {
         data = mGsmService.getData();
         return data;
+    }
+
+    /**
+     * Get distance b/w two points namely source and destination
+     * @param src
+     * @param dest
+     * @return double distance value
+     */
+    public double getDistance(GsmData src, GsmData dest) {
+        Location locationA = new Location("point A");
+
+        locationA.setLatitude(src.getGsmLat());
+        locationA.setLongitude(src.getGsmLong());
+
+        Location locationB = new Location("point B");
+
+        locationB.setLatitude(dest.getGsmLat());
+        locationB.setLongitude(dest.getGsmLong());
+
+        float distance = locationA.distanceTo(locationB);
+        return ((double) distance);
     }
 
 }

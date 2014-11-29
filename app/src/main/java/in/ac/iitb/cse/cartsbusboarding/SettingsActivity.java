3e14de0088ca0929b9eae22241ef8a8867fb7751
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-11-30 02:12:49 +0530

    Fix binding error for accuracy field in preferences

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/SettingsActivity.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/SettingsActivity.java
@@ -86,6 +86,7 @@ public class SettingsActivity extends PreferenceActivity {
         // their values. When their values change, their summaries are updated
         // to reflect the new value, per the Android Design guidelines.
         bindPreferenceSummaryToValue(findPreference("sync_frequency"));
+        bindPreferenceSummaryToValue(findPreference("accuracy"));
     }
 
     /**

e01cc3efad82ca05bc9d353e47aaa6f9f706cde3
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-12-06 23:41:15 +0530

    Added licensing information to all files

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/PollingService.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/PollingService.java
@@ -1,3 +1,26 @@
+/**
+ *   CartsBusBoarding - Bus Boarding Event detection project by
+ *                      CARTS in IITB & UIET, Panjab University
+ *
+ *   Copyright (c) 2014 Shubham Chaudhary <me@shubhamchaudhary.in>
+ *   Copyright (c) 2014 Tanjot Kaur <tanjot28@gmail.com>
+ *
+ *   This file is part of CartsBusBoarding.
+ *
+ *   CartsBusBoarding is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   CartsBusBoarding is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with CartsBusBoarding.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package in.ac.iitb.cse.cartsbusboarding;
 
 import android.app.Service;

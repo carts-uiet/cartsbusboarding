d89ba5d837045069bfa0bacb45cdddc330863c24
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-10 16:05:27 +0530

    Enable unit test support

---
+++ app/src/test/java/in/ac/iitb/cse/cartsbusboarding/DummyTest.java
@@ -0,0 +1,16 @@
+package in.ac.iitb.cse.cartsbusboarding;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+
+public class DummyTest {
+    @Test
+    public void testShouldPass() {
+        assertThat(true, is(true));
+    }
+}

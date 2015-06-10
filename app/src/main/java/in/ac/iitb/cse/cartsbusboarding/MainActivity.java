87476f3819393d3bbe1e7df7d2dcefa08c7426c0
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-10 21:42:57 +0530

    Remove pager & use simple fragment

--- app/src/main/java/in/ac/iitb/cse/cartsbusboarding/MainActivity.java
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/MainActivity.java
@@ -28,14 +28,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.view.*;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import in.ac.iitb.cse.cartsbusboarding.acc.AccEngine;
@@ -47,8 +46,7 @@ import in.ac.iitb.cse.cartsbusboarding.gsm.GsmEngine;
 import in.ac.iitb.cse.cartsbusboarding.gsm.GsmListener;
 import in.ac.iitb.cse.cartsbusboarding.tasks.AccDisplayTask;
 import in.ac.iitb.cse.cartsbusboarding.tasks.GsmDisplayTask;
-
-import java.util.Locale;
+import in.ac.iitb.cse.cartsbusboarding.utils.MainFragment;
 
 public class MainActivity extends AppCompatActivity implements AccDisplayController {
 
@@ -63,33 +61,13 @@ public class MainActivity extends AppCompatActivity implements AccDisplayControl
     private SwipeRefreshLayout accRefreshLayout;
     private SwipeRefreshLayout gsmRefreshLayout;
 
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        MainFragment fragment = new MainFragment();
+        supportFragmentManager.beginTransaction().add(android.R.id.content, fragment).commit();
 
         /* Our Stuff */
         init_gsm();
@@ -209,40 +187,6 @@ public class MainActivity extends AppCompatActivity implements AccDisplayControl
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            //This is called once the fragments have been loaded
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
     @Override
     public void displayGsm(GsmDisplayData data) {
         TextView twData = (TextView) findViewById(R.id.section_data_gsm);
@@ -284,44 +228,6 @@ public class MainActivity extends AppCompatActivity implements AccDisplayControl
                         + "Entropy: " + String.format(format, data.getEntropy()))
         );
         accRefreshLayout.setRefreshing(false);
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 1;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
     }
 
 }
---
+++ app/src/main/java/in/ac/iitb/cse/cartsbusboarding/utils/MainFragment.java
@@ -0,0 +1,16 @@
+package in.ac.iitb.cse.cartsbusboarding.utils;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import in.ac.iitb.cse.cartsbusboarding.R;
+
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
